f64225c1bb8663d1882e42e3ee1286c57dc0b412
Benjamin Mikael Andersen <benjadcool@gmail.com>
2024-04-08 14:14:41 +0200

    Insert ExcelData for BR18 and Classes for calculating LCA

---
+++ org.sdu.dsl4ifc.parent/org.sdu.dsl4ifc/src/lca/LCA/LCA.java
@@ -0,0 +1,77 @@
+package lca.LCA;
+
+import java.util.ArrayList;
+
+import lca.epdConnectors.BR18Connector;
+import lca.Interfaces.IEPDConnector;
+import lca.Interfaces.IEnvProductInfo;
+
+public class LCA {
+    private LCACalc lcaCalc;
+    private IEPDConnector edpConnetcor;
+
+
+    public LCA(){
+        lcaCalc = new LCACalc();
+        edpConnetcor = new BR18Connector();
+    }
+
+
+    public double CalculateLCAForWall(double quantity, double aRef) {
+
+        double a = 53.1 * quantity;
+        double c3 = 0.965 * quantity;
+        double c4 = 0.7 * quantity;
+
+        return lcaCalc.CalculateLCABasic(a,c3,c4,aRef);
+    }
+
+    public double CalculateLCAForElement(LCAIFCElement element, double area) {
+
+        IEnvProductInfo envProductInfo = edpConnetcor.GetEPDDataByType(element.getName());
+
+        element.setaResult(MultiplyWithQuanitities(envProductInfo.getA(),element));
+        element.setC3Result(MultiplyWithQuanitities(envProductInfo.getC3(),element));
+        element.setC4Result(MultiplyWithQuanitities(envProductInfo.getC4(),element));
+
+        double aRes = TranslateNullToZero(element.getaResult());
+        double c3Res = TranslateNullToZero(element.getC3Result());
+        double c4Res = TranslateNullToZero(element.getC4Result());
+
+        return lcaCalc.CalculateLCABasic(aRes, c3Res, c4Res, area);
+    }
+
+    private double TranslateNullToZero(Double d) {
+        if (d == null) {
+            return 0;
+        }
+
+        return d;
+    }
+
+    private Double MultiplyWithQuanitities(Double envInfo, LCAIFCElement element) {
+        if (envInfo == null) {
+            return null;
+        }
+
+        int yearFactor = 1;
+        if (element.getLifeTime() >= 1) {
+            yearFactor = (int) Math.ceil(50 / element.getLifeTime());
+        }
+
+        return envInfo * element.getQuantity() * yearFactor;
+    }
+
+    public LCAResult CalculateLCAWhole(ArrayList<LCAIFCElement> ifcElements, double area, double areaHeated, double b6) {
+
+        for (LCAIFCElement element : ifcElements) {
+            element.setLcaVal(CalculateLCAForElement(element, area));
+        }
+
+        double baseResult = ifcElements.stream().mapToDouble(LCAIFCElement::getLcaVal).sum();
+        double opResult = lcaCalc.CalculateLCAOperational(b6, areaHeated);
+        double result = baseResult + opResult;
+
+        return new LCAResult(result,ifcElements);
+    }
+}
--- org.sdu.dsl4ifc.parent/org.sdu.dsl4ifc/src/org/sdu/dsl4ifc/generator/depedencyGraph/blocks/LcaBlock.java
+++ org.sdu.dsl4ifc.parent/org.sdu.dsl4ifc/src/org/sdu/dsl4ifc/generator/depedencyGraph/blocks/LcaBlock.java
@@ -1,6 +1,9 @@
 package org.sdu.dsl4ifc.generator.depedencyGraph.blocks;
 
 import org.sdu.dsl4ifc.generator.depedencyGraph.core.Block;
+import java.util.ArrayList;
+import java.util.Map;
+import lca.LCA.*;
 
 public class LcaBlock extends Block<String> {
 
@@ -17,7 +20,50 @@ public class LcaBlock extends Block<String> {
 
 	@Override
 	public String Calculate() {
-		// TODO Auto-generated method stub
+		
+		LCA lca = new LCA();
+
+        String concrete = "Letbeton vægelement, 150 mm tyk væg, 10% udsparinger";
+        String floorS = "Celleglas-isolering 115 kg/m³";
+
+        LCAIFCElement wall1 = new LCAIFCElement(concrete, 200);
+        wall1.setLifeTime(12);
+        LCAIFCElement wall2 = new LCAIFCElement(concrete, 200);
+        wall2.setLifeTime(70);
+        LCAIFCElement wall3 = new LCAIFCElement(concrete, 200);
+        LCAIFCElement wall4 = new LCAIFCElement(concrete, 200);
+
+        LCAIFCElement floor = new LCAIFCElement(floorS, 1000);
+
+        ArrayList<LCAIFCElement> elements = new ArrayList<>();
+        elements.add(wall1);
+        elements.add(wall2);
+        elements.add(wall3);
+        elements.add(wall4);
+        elements.add(floor);
+
+        LCAResult lcaResult = lca.CalculateLCAWhole(elements, 200, 180, 1000);
+
+        LCAIFCElement br18 = lcaResult.getElements().stream().filter(e -> e.getName().equals(floorS)).findAny().orElse(null);
+
+        System.out.println(br18);
+
+        System.out.println("LCA.LCA for building = " + lcaResult.getLcaResult() + " kg CO2-equivalents/m2/year");
+
+
+        lcaResult.getElements().forEach(e -> {
+            Map<String, Double> map = e.getResultMap();
+            System.out.println("{ Name: " + e.getName() + " With Quantity: " + e.getQuantity());
+            System.out.println("    LCA for A1-A3 + C3 & C4: " + e.getLcaVal());
+
+            for (Map.Entry<String, Double> set : map.entrySet()) {
+                if (set.getValue() == null) {
+                    System.out.println("   " + set.getKey() + " equals null");
+                }
+            }
+            System.out.println("}");
+        });
+		
 		return null;
 	}
 
