0cf2e1981e1746e113cdc37dc40a1d87861993f1
Benjamin Mikael Andersen <benjadcool@gmail.com>
2024-04-29 14:07:27 +0200

    Create LcaIfcQuantity class

---
+++ org.sdu.dsl4ifc.parent/org.sdu.dsl4ifc/src/lca/LCA/LcaIfcQuantity.java
@@ -0,0 +1,28 @@
+package lca.LCA;
+
+public class LcaIfcQuantity {
+	
+	private Double grossSideArea;
+	private Double grossVolume;
+	
+	public LcaIfcQuantity(Double grossSideArea, Double grossVolume) {
+		this.grossSideArea = grossSideArea;
+		this.grossVolume = grossVolume;
+	}
+
+	public Double getGrossSideArea() {
+		return grossSideArea;
+	}
+
+	public void setGrossSideArea(Double grossSideArea) {
+		this.grossSideArea = grossSideArea;
+	}
+
+	public Double getGrossVolume() {
+		return grossVolume;
+	}
+
+	public void setGrossVolume(Double grossVolume) {
+		this.grossVolume = grossVolume;
+	}
+}
