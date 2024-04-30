cfaa939784c7a040ba56b0c2a86d2a8c3fa1c93d
Benjamin Mikael Andersen <benjadcool@gmail.com>
2024-04-30 15:59:50 +0200

    Create Wrapper object classes for Json Objects

---
+++ org.sdu.dsl4ifc.parent/org.sdu.dsl4ifc/src/lca/epdConnectors/JsonWrappers/LciaMethRefJsonObject.java
@@ -0,0 +1,37 @@
+package lca.epdConnectors.JsonWrappers;
+
+public class LciaMethRefJsonObject {
+    private String type;
+    private String refObjectId;
+    private String version;
+    
+    public LciaMethRefJsonObject(String type, String refObjectId, String version) {
+    	this.type = type;
+    	this.refObjectId = refObjectId;
+    	this.version = version;
+	}
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getRefObjectId() {
+        return refObjectId;
+    }
+
+    public void setRefObjectId(String refObjectId) {
+        this.refObjectId = refObjectId;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+}
