2778eaf1477a9257ea99e90ce338478068af982d
Benjamin Mikael Andersen <benjadcool@gmail.com>
2024-05-08 10:09:02 +0200

    Implement cache for both EPD metadata and EnvProductInfo

--- org.sdu.dsl4ifc.parent/org.sdu.dsl4ifc/src/lca/epdConnectors/EcoPlatformConnector.java
+++ org.sdu.dsl4ifc.parent/org.sdu.dsl4ifc/src/lca/epdConnectors/EcoPlatformConnector.java
@@ -35,16 +35,13 @@ public class EcoPlatformConnector implements IEPDConnector {
 	private final String flowPropUnitId = "93a60a56-a3c8-22da-a746-0800200c9a66";
 	private final int unitDataSetId = 1;
 	private String bearerToken = "eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJCZW5qaTcwMyIsImlzcyI6IkVDT1BPUlRBTCIsImF1ZCI6ImFueSIsInZlciI6IjcuOS40IiwicGVybWlzc2lvbnMiOlsic3RvY2s6cmVhZCxleHBvcnQ6MiIsInN0b2NrOnJlYWQsZXhwb3J0OjEiLCJ1c2VyOnJlYWQsd3JpdGU6NjcxIl0sInJvbGVzIjpbXSwiaWF0IjoxNzExMTAwMjE5LCJleHAiOjE3MTg5ODQyMTksImVtYWlsIjoiYmVhbmQxOUBzdHVkZW50LnNkdS5kayIsInRpdGxlIjoiIiwiZmlyc3ROYW1lIjoiQmVuamFtaW4iLCJsYXN0TmFtZSI6IkFuZGVyc2VuIiwiZ2VuZXJhdGVOZXdUb2tlbnMiOmZhbHNlLCJqb2JQb3NpdGlvbiI6IkthbmRpZGF0c3R1ZGVyZW5kZSIsImFkZHJlc3MiOnsiY2l0eSI6Ik9kZW5zZSIsInppcENvZGUiOiI1MjQwIiwiY291bnRyeSI6IkRLIiwic3RyZWV0IjoiIn0sIm9yZ2FuaXphdGlvbiI6e30sInVzZXJHcm91cHMiOlt7InVzZXJHcm91cE5hbWUiOiJyZWdpc3RlcmVkX3VzZXJzIiwidXNlckdyb3VwT3JnYW5pemF0aW9uTmFtZSI6IkRlZmF1bHQgT3JnYW5pemF0aW9uIn1dLCJhZG1pbmlzdHJhdGVkT3JnYW5pemF0aW9uc05hbWVzIjoiIiwicGhvbmUiOiIiLCJkc3B1cnBvc2UiOiJUaWwgZXQga2FuZGlkYXRwcm9qZWt0IHZlZHLDuHJlbmRlIGVuIElULXBsYXRmb3JtIHRpbCB1ZHJlZ25pbmcgYWYgYmxhLiBMQ0EiLCJzZWN0b3IiOiIiLCJpbnN0aXR1dGlvbiI6IlN5ZGRhbnNrIFVuaXZlcnNpdGV0In0.tidxKWZu6Nz9z4GgSAPRO85dHjaWpIvCxX9bM0lxGSPIDw4SSRgdbLeSGfvck6nG6YqChG_Flr32iQq-QMIGNkzchtxkkWg3uupmtyYhJAQINWwEo5Pjh1LkO5Gh3cMN5LhMoT_qXFZs2B7DsEA6V2RQpMPfKTflm8p47wl9BKU";
-	private Map<String, EnvProductInfo> envProdMap;
-	private List<EpdMetaDataJsonObject> epdMetaDataList;
+	private static Map<String, EnvProductInfo> envProdMap  = new HashMap<String, EnvProductInfo>();;
+	private static List<EpdMetaDataJsonObject> epdMetaDataList = new ArrayList<EpdMetaDataJsonObject>();
 	
 	public EcoPlatformConnector() {
 		GsonBuilder gsonBuilder = new GsonBuilder();
 		gsonBuilder.registerTypeAdapter(AniesJsonObject.class, new CustomDeserializerAnies());
 		gson = gsonBuilder.create();
-		
-		envProdMap = new HashMap<String, EnvProductInfo>();
-		epdMetaDataList = new ArrayList<EpdMetaDataJsonObject>();
 	}
 	
 	@Override
@@ -58,9 +55,12 @@ public class EcoPlatformConnector implements IEPDConnector {
 			return envProdMap.get(name);
 		}
 		
+
 		if (epdMetaDataList.isEmpty()) {
 			epdMetaDataList = getEpdDatabaseObjectList();
 		}
+
+
 		
 		EpdMetaDataJsonObject epdObject = getEpdDatabaseObjectFromLocalList(name);
 		
@@ -71,6 +71,7 @@ public class EcoPlatformConnector implements IEPDConnector {
 		String uri = epdObject.getUri();
 		
 		EpdSpecificProductJson epdProduct = fetchEpdData(uri);
+		
 		if (epdProduct == null) {
 			return null;
 		}
@@ -123,13 +124,10 @@ public class EcoPlatformConnector implements IEPDConnector {
 	}
 	
     private EpdMetaDataJsonObject getEpdDatabaseObjectFromLocalList(String name) {
-    	
-    	long before = System.currentTimeMillis();
-    	
-    	for (EpdMetaDataJsonObject e : this.epdMetaDataList) {
+
+    	for (EpdMetaDataJsonObject e : epdMetaDataList) {
     		if (e.getName() != null && e.getName().equals(name)) {
-    			long after = System.currentTimeMillis();
-    	    	System.out.println("Fetching edp from list: " + (after - before));
+
     			return e;
     		}
     	}
