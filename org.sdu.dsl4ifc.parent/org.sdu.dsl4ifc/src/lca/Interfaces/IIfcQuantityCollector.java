1808a9fa8ec095b50e931b3705b72a0d62eb1bed
Benjamin Mikael Andersen <benjadcool@gmail.com>
2024-05-08 14:27:14 +0200

    Implement getQuantity for IfcWall

--- org.sdu.dsl4ifc.parent/org.sdu.dsl4ifc/src/lca/Interfaces/IIfcQuantityCollector.java
+++ org.sdu.dsl4ifc.parent/org.sdu.dsl4ifc/src/lca/Interfaces/IIfcQuantityCollector.java
@@ -2,9 +2,10 @@ package lca.Interfaces;
 
 import com.apstex.ifc2x3toolbox.ifc2x3.IfcBuildingElement;
 
+import lca.DomainClasses.Enums.DeclaredUnitEnum;
+import lca.LCA.LcaIfcQuantity;
+
 public interface IIfcQuantityCollector<IfcBuildingElement> {
 	
-	public Double getVolume(IfcBuildingElement IfcBuildingElement);
-	
-	public Double getArea(IfcBuildingElement IfcBuildingElement);
+	public Double getQuantity(IfcBuildingElement IfcBuildingElement, DeclaredUnitEnum unit);
 }
--- org.sdu.dsl4ifc.parent/org.sdu.dsl4ifc/src/lca/ifc/IfcWallQuantityCollector.java
+++ org.sdu.dsl4ifc.parent/org.sdu.dsl4ifc/src/lca/ifc/IfcWallQuantityCollector.java
@@ -1,21 +1,90 @@
 package lca.ifc;
 
+import com.apstex.ifc2x3toolbox.ifc2x3.IfcElementQuantity;
+import com.apstex.ifc2x3toolbox.ifc2x3.IfcPhysicalQuantity;
+import com.apstex.ifc2x3toolbox.ifc2x3.IfcQuantityArea;
+import com.apstex.ifc2x3toolbox.ifc2x3.IfcQuantityVolume;
+import com.apstex.ifc2x3toolbox.ifc2x3.IfcRelDefines;
+import com.apstex.ifc2x3toolbox.ifc2x3.IfcRelDefinesByProperties;
 import com.apstex.ifc2x3toolbox.ifc2x3.IfcWall;
+import com.apstex.step.core.SET;
 
+import lca.DomainClasses.Enums.DeclaredUnitEnum;
 import lca.Interfaces.IIfcQuantityCollector;
+import lca.LCA.LcaIfcQuantity;
 
 public class IfcWallQuantityCollector implements IIfcQuantityCollector<IfcWall> {
 
 	@Override
-	public Double getVolume(IfcWall IfcWall) {
-		// TODO Auto-generated method stub
-		return null;
+	public Double getQuantity(IfcWall ifcWall, DeclaredUnitEnum unit) {
+		Double quantity = null;
+		var invSet = ifcWall.getIsDefinedBy_Inverse();
+		if (invSet == null) {
+			return null;
+		}
+		
+		IfcElementQuantity elementQuant = getElementQuant(invSet);
+		if (elementQuant == null) {
+			return null;
+		}
+		
+		switch (unit) {
+		case M3:
+			quantity = getGrossVolume(elementQuant);
+			break;
+		case M2:
+			quantity = getGrossArea(elementQuant);
+			break;
+		default:
+			break;
+		}
+		
+		return quantity;
 	}
 
-	@Override
-	public Double getArea(IfcWall IfcWall) {
-		// TODO Auto-generated method stub
-		return null;
+	private IfcElementQuantity getElementQuant(SET<IfcRelDefines> invSet) {
+		IfcElementQuantity elementQuant = null;
+		
+		for (IfcRelDefines iRel : invSet) {
+			
+			if (!(iRel instanceof IfcRelDefinesByProperties)) {
+				continue;
+			}
+			
+			var iRelProp = (IfcRelDefinesByProperties)iRel;
+			
+			if (!(iRelProp.getRelatingPropertyDefinition() instanceof IfcElementQuantity)) {
+				continue;
+			}
+			
+			elementQuant = (IfcElementQuantity)iRelProp.getRelatingPropertyDefinition();
+		}
+		return elementQuant;
 	}
+	
+	private Double getGrossVolume(IfcElementQuantity elementQuant) {
+		double grossVolume = 0;
+		
+		for (IfcPhysicalQuantity q : elementQuant.getQuantities()) {
+			if (q instanceof IfcQuantityVolume && q.getName().getDecodedValue().equals("GrossVolume")) {
+				grossVolume = ((IfcQuantityVolume)q).getVolumeValue().getValue();
+			}
+		}
+		
+		return grossVolume;
+	}
+	
+	private Double getGrossArea(IfcElementQuantity elementQuant) {
+		double grossArea = 0;
+		
+		for (IfcPhysicalQuantity q : elementQuant.getQuantities()) {
+			if (q instanceof IfcQuantityArea && q.getName().getDecodedValue().equals("GrossArea")) {
+				grossArea = ((IfcQuantityArea)q).getAreaValue().getValue();
+			}
+		}
+		return grossArea;
+	}
+
+
 
 }
